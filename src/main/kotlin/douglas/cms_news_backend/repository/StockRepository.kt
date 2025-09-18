21e118963647d706eea9fb468f8137d7ee76b5d3
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-18 11:29:18 -0300

    feat: add user-agent browser

--- src/main/kotlin/douglas/cms_news_backend/repository/StockRepository.kt
+++ src/main/kotlin/douglas/cms_news_backend/repository/StockRepository.kt
@@ -17,12 +17,15 @@ class StockRepository(
     private val limit = 7
     private val range = "1mo"
     private val interval = "1d"
+    val browserUserAgent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36" +
+            " (KHTML, like Gecko) Chrome/140.0.0.0 Mobile Safari/537.36"
     private val webClient = webClientBuilder.baseUrl(apiUrl).build()
 
     fun fetchStockCodes(sortBy: String, sortOrder: String): List<String> {
         val resourceUri = "list?type=stock&sortBy=$sortBy&sortOrder=$sortOrder&limit=$limit&token=$apiToken"
         val response = webClient.get()
             .uri(resourceUri)
+            .header("User-Agent", browserUserAgent)
             .retrieve()
             .bodyToMono(BrapiResponse::class.java)
             .block()
@@ -35,6 +38,7 @@ class StockRepository(
                 val resourceUri = "$code?range=$range&interval=$interval&token=$apiToken"
                 webClient.get()
                     .uri(resourceUri)
+                    .header("User-Agent", browserUserAgent)
                     .retrieve()
                     .bodyToMono(QuoteResponse::class.java)
                     .mapNotNull { it.results.firstOrNull() }
